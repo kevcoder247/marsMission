0f3ecda9798154c6997720367349be2eea5556ad
kevin ventura <kevcoder@kevins-mbp.myfiosgateway.com>
2023-06-02 21:05:31 -0400

    wip: show page added, we can now view each mission individually when like is clicked

--- server.js
+++ server.js
@@ -9,9 +9,21 @@ app.get('/missions/', (req, res) => {
   res.render('index.ejs', {missions})
 
   //console.log(missions)
-})
+});
+
+
+
+//SHOW
+app.get('/missions/:id', (req, res) =>{
+  console.log(missions[req.params.id])
+  const currentMission = missions[req.params.id];
+  res.render('show.ejs', {missions: currentMission})
+});
+
+
+
 
 app.listen(PORT, () => {
   console.log(`App is listening on port: ${PORT}`)
-})
+});
 
